227a0ec84881a75963518b1ce3b653197408437b
Mauricio Contreras Canepa <virtual.kur2@gmail.com>
2025-07-15 08:47:22 +0200

    feat: implement CreateUser use case and password hashing service
    introduce `CreateUserUseCase` for user registration logic, including validation and password hashing. add `IPasswordHashService` interface for password management. also, implement `AuthController` for handling user signup and login requests with appropriate error handling.
    
    refs: user registration and authentication

---
+++ src/application/use-cases/CreateUser.ts
@@ -0,0 +1,33 @@
+import { User } from "../../domain/entities/User";
+import { IUserRepository } from "../../domain/repositories/IUserRespository";
+import { IPasswordHashService } from "../../domain/services/PasswordHashService";
+import { CreateUserDto } from "../dto/CreateUser.dto";
+import { v4 as uuidv4} from 'uuid';
+
+export class CreateUserUseCase {
+    constructor(
+        private readonly userRepository: IUserRepository,
+        private readonly passwordHasher: IPasswordHashService,
+    ) {}
+
+    async execute(data: CreateUserDto): Promise<User> {
+        if(!(data.name && data.email && data.password)) {
+            throw new Error('Name, email and password are required');
+        }
+
+        const existingUser = await this.userRepository.findByEmail(data.email);
+
+        if(existingUser) {
+            throw new Error('Can\'t create user');  // no info regarding existing user is shown to the client
+        }
+
+        const hashedPassword = await this.passwordHasher.hash(data.password);
+        const newUser = new User(uuidv4(), data.name, data.email, hashedPassword);
+
+        const savedUser = await this.userRepository.save(newUser);
+
+        const userWithoutPassword = new User(savedUser.id, savedUser.name, savedUser.email);
+        
+        return userWithoutPassword;
+    }
+}
